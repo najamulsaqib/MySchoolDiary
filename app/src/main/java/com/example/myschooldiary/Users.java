bfa616ef6451925035779d05345837db6ac7ad3e
Najam Ul Saqib <1najamulsaqib@gmail.com>
2021-04-03 18:34:45 +0500

    Login Done

---
+++ app/src/main/java/com/example/myschooldiary/Users.java
@@ -0,0 +1,13 @@
+package com.example.myschooldiary;
+
+import android.content.Context;
+
+public class User {
+    String Name, Email, Code;
+    User(){}
+    User(String Name, String Email, String Code){
+        this.Name = Name;
+        this.Email = Email;
+        this.Code = Code;
+    }
+}
